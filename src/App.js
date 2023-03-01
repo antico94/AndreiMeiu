39de1ae7f1babb15b28ed0acc3d75fd8a5b9c163
antico94 <meiuandrei@gmail.com>
2023-03-01 13:13:52 +0200

    tesT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Meiu from './assets/me.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hire Me!</h1>
+        <img src={Meiu} alt='eu'/>
+        <h3>Everybody say: Andrei very good. Very hardworking, he make only good job.</h3>
+
     </div>
   );
 }
