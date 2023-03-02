3e436718cd718533220997010a147914ff2ffad1
antico94 <meiuandrei@gmail.com>
2023-03-02 21:44:43 +0200

    added transition effects

--- src/components/social/social.jsx
+++ src/components/social/social.jsx
@@ -14,7 +14,7 @@ const Social = () => {
                         strokeWidth={1}
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        className={styles.githubSvg}
+                        className={`${styles.githubSvg}  ${styles.logoIcon}`}
                     >
                         <title>{"GitHub"}</title>
                         <path
@@ -32,7 +32,7 @@ const Social = () => {
                         strokeWidth={1}
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        className={styles.linkedInSvg}
+                        className={`${styles.linkedInSvg}  ${styles.logoIcon}`}
                     >
                         <title>{"LinkedIn"}</title>
                         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z" />
